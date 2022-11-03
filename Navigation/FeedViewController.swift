78ddc3594317fbef0ce028dfd2ad88b3b591c327
yudindennis <93090007+yudindennis@users.noreply.github.com>
2022-11-03 16:01:14 +0500

    Add files via upload
    hwdone

---
+++ Navigation/FeedViewController.swift
@@ -0,0 +1,41 @@
+//
+//  FeedViewController.swift
+//  Navigation
+//
+//  Created by Денис Юдин on 24.10.2022.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray
+        let uiButton = UIButton()
+        self.view.addSubview(uiButton)
+        uiButton.setTitle("Пост", for: .normal)
+        uiButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        uiButton.addAction(UIAction(handler: {_ in
+            let vc = PostViewController()
+            self.navigationController?.pushViewController(vc, animated: true)
+        }), for: .touchUpInside)
+        
+        NSLayoutConstraint.activate([
+            uiButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            uiButton.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            uiButton.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            uiButton.heightAnchor.constraint(equalToConstant: 30)])
+        
+       
+        }
+            
+        
+        
+
+    }
+    
+
+
