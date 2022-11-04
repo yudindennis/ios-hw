082be0fc7ba0b18632a3ad038e1d9188df0e5cd8
Yudin Denis <yudindennis@icloud.com>
2022-11-04 17:16:00 +0500

    hw

---
+++ Netology_IB_Instruments 2/Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,33 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Denis Yudin on 22.09.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var photo: UIImageView!
+    
+    @IBOutlet weak var name: UILabel!
+    
+    @IBOutlet weak var dateOfBirth: UILabel!
+    
+    @IBOutlet weak var city: UILabel!
+    
+    @IBOutlet weak var text: UITextView!
+    
+    
+    
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Netology_IB_Instruments 2/Netology_IB_Instruments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Denis Yudin on 22.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
